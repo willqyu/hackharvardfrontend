35861be31a58d672289041790715ca0678f97c46
Anushka De <anushkad@stanford.edu>
2024-10-13 03:25:38 -0400

    Create maps.tsx

---
+++ app/ui/maps.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const SlidingReel = () => {
+    const [expandedBox, setExpandedBox] = useState(null);
+    const htlpFiles = ['map1.html', 'map2.html', 'map3.html'];
+    
+}
+
+export default function Home() {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen bg-gradient-to-tr from-slate-800 to-slate-950  bg-pink-300">  
+
+    </div>
+  );
+}
