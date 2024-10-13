82b43eef3303c06c2fb04321faae535c8b44fa93
Anushka De <anushkad@stanford.edu>
2024-10-13 06:10:34 -0400

    tweets

--- app/tweets/GradientGauge.tsx
+++ app/tweets/GradientGauge.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 
+
 const GradientGauge = ({ value }) => {
   // Ensure the value stays within the 0-10 range
   const clampedValue = Math.max(0, Math.min(value, 10));
@@ -15,19 +16,19 @@ const GradientGauge = ({ value }) => {
 
   return (
     <div className="flex flex-col items-center justify-center my-10">
-      <div className="relative w-72 h-8 rounded-lg overflow-hidden">
+      <div className="relative w-72 h-8">
         {/* Gradient background */}
-        <div className="absolute inset-0 bg-gradient-to-r from-red-500 to-green-500"></div>
+        <div className="absolute rounded-xl inset-0 bg-gradient-to-r from-red-500 to-green-500 shadow-md shadow-slate-700"></div>
         {/* Needle */}
         <div
           ref={needleRef} // Attach the ref to the needle
-          className="absolute w-1 h-8 bg-black transition-left duration-300 ease-in-out" // Add transition for left property
+          className="absolute w-1 h-8 bg-white transition-left duration-300 ease-in-out" // Add transition for left property
           style={{
             transform: 'translateX(-50%)', // Center the needle on its position
           }}
         />
       </div>
-      <div className="mt-2 text-xl font-bold">{clampedValue.toFixed(1)}</div>
+      <div className="mt-2 text-xl text-white font-bold">{clampedValue.toFixed(1)}</div>
     </div>
   );
 };
