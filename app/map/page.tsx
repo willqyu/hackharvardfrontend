4efc84b9c793252bfdbf4c4ff3610780ad1c27be
Anushka De <anushkad@stanford.edu>
2024-10-13 06:36:13 -0400

    report dashboard

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -10,16 +10,24 @@ const MapComponent = dynamic(() => import('./MapComponent'), {
 
 export default async function MapPage() {
   const mapData = await getMapData();
-  console.log(typeof(mapData[0].image))
+  // console.log(typeof(mapData[0].image))
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4"></h1>
-      <div style={{ height: '600px', width: '100%' }}>
+    <div className="bg-gradient-to-tr from-slate-800 to-slate-950">
+      <div className="container mx-auto p-4">
+      <h1 className="mt-6 mb-6 text-center text-5xl text-white shadow-md font-bold"> 
+        Report dashboard 
+      </h1>
+      <p className="text-white mb-5 text-center text-lg">
+        Zoom in to see where constituents are reporting issues. 
+      </p>
+      <div className="h-[600px] w-full rounded-2xl overflow-hidden">
         <Suspense fallback={<div>Loading map...</div>}>
           <MapComponent mapData={mapData} />
         </Suspense>
       </div>
+
+    </div>
     </div>
   );
 }
